66b03d3846857edb55d51ed7817deb05ec291248
Sargis Aghabekyan <s.aghabekyan@yandex.ru>
2018-12-12 19:15:49 +0400

    added users list page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models').User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'Index' });
+});
+
+/* GET Users page. */
+router.get('/users', function(req, res, next) {
+  var xxx
+  xxx = User
+      .findAll({
+          attributes: ['id', 'firstName', 'lastName']
+        })
+  xxx.then((users) => res.render('users', { title: 'Users', 'users': users }))
+
+  // res.render('users', { title: 'Users', 'xxx': xxx })
 });
 
 module.exports = router;
